09212515f4b89122c62183b50e63fa018f4d7221
jmcasabona <casaxmore@gmail.com>
2020-02-19 12:01:58 +0100

    Código incluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newNgifElse';
+  usuario1: string;
+  usuario2: string;
 }
